6a79343b9bdf1f1abcf1691e85948064e6db0865
jiangzhangy <649768017@qq.com>
2019-06-23 02:23:00 +0000

    话题排序

--- app/helpers.php
+++ app/helpers.php
@@ -7,5 +7,5 @@ function route_class()
 
 function category_nav_active($category_id)
 {
-    return active_class((if_route('categories.show') && if_route_param('category', $category->id)));
+    return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
 }
