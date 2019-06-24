f54ba796d3f812d7602493d86fe0eb66e9e0b3fa
jiangzhangy <649768017@qq.com>
2019-06-24 02:29:10 +0000

    修复 XSS 注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -19,8 +19,11 @@ class TopicObserver
         //
     }
 
-    public function svaing(Topic $topic)
+    public function saving(Topic $topic)
     {
+        //防 xss 攻击过滤
+        $topic->body = clean($topic->body, 'user_topic_body');
+        //自定义函数截取摘要
         $topic->excerpt = make_excerpt($topic->body);
     }
 }
